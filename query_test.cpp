768c78996b3b98f33b6ccfbf7d09bc268303114f
nishiba <nishiba.m@gmail.com>
2016-05-01 15:06:30 +0900

    updated

--- query_test.cpp
+++ query_test.cpp
@@ -4,14 +4,14 @@
 
 TEST(Query, skipTest)
 {
-    const std::vector<int> x = { 0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9 };
+    std::vector<int> x = range(0, 10).toStdVector();
     const std::vector<int> y = query(x).skip(2).toStdVector();
     EXPECT_EQ(x[2], y[0]);
 }
 
 TEST(Query, takeTest)
 {
-    const std::vector<int> x = { 0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9 };
+    std::vector<int> x = range(0, 10).toStdVector();
     const std::vector<int> y = query(x).take(2).toStdVector();
     EXPECT_EQ(2, y.size());
     EXPECT_EQ(x[1], y[1]);
@@ -19,8 +19,8 @@ TEST(Query, takeTest)
 
 TEST(Query, zipTest)
 {
-    const std::vector<int> x = { 0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9 };
-    const std::vector<int> y = { 1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10 };
+    std::vector<int> x = range(0, 10).toStdVector();
+    std::vector<int> y = range(1, 11).toStdVector();
     const auto z = zip(x, y).toStdVector();
     EXPECT_EQ(x[0], z[0].get<0>());
     EXPECT_EQ(y[0], z[0].get<1>());
@@ -29,7 +29,7 @@ TEST(Query, zipTest)
 
 TEST(Query, selectTest)
 {
-    const std::vector<int> x = {0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9};
+    std::vector<int> x = range(0, 10).toStdVector();
     auto f = [](auto& a) {return a + 1; };
     const std::vector<int> y = query(x).select(f).toStdVector();
     EXPECT_EQ(y[0], f(x[0]));
@@ -38,7 +38,7 @@ TEST(Query, selectTest)
 
 TEST(Query, selectWithIndexTest)
 {
-    const std::vector<int> x = { 0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9 };
+    std::vector<int> x = range(0, 10).toStdVector();
     auto f = [](const int& a, std::size_t i) {return a + 1; };
     const std::vector<int> y = query(x).selectWithIndex(f).toStdVector();
     EXPECT_EQ(f(x[0], 0), y[0]);
@@ -47,8 +47,8 @@ TEST(Query, selectWithIndexTest)
 
 TEST(Query, selectUnzipTest)
 {
-    const std::vector<int> x = { 0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9 };
-    const std::vector<int> y = { 1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 0 };
+    std::vector<int> x = range(0, 10).toStdVector();
+    std::vector<int> y = range(1, 11).toStdVector();
     const std::vector<int> z = zip(x, y)
         .selectUnzip([](int a, int b) {return a + b; })
         .toStdVector();
@@ -59,7 +59,7 @@ TEST(Query, selectUnzipTest)
 
 TEST(Query, applyTest)
 {
-    std::vector<int> x = { 0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9 };
+    std::vector<int> x = range(0, 10).toStdVector();
     auto f = [](auto& a) {a += 1; };
     query(x).apply(f);
     EXPECT_EQ(1, x[0]);
@@ -68,7 +68,7 @@ TEST(Query, applyTest)
 
 TEST(Query, applyWithIndexTest)
 {
-    std::vector<int> x = { 0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9 };
+    std::vector<int> x = range(0, 10).toStdVector();
     auto f = [](auto& a, std::size_t i) {a += i; };
     query(x).applyWithIndex(f);
     EXPECT_EQ(0, x[0]);
@@ -77,19 +77,51 @@ TEST(Query, applyWithIndexTest)
 
 TEST(Query, applyUnzipTest)
 {
-    std::vector<int> x = { 0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9 };
-    std::vector<int> y = { 1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 0 };
+    std::vector<int> x = range(0, 10).toStdVector();
+    std::vector<int> y = range(1, 11).toStdVector();
     zip(x, y).applyUnzip([](int& a, int& b) { a += 1; b += 1; });
     EXPECT_EQ(2, x[1]);
     EXPECT_EQ(3, y[1]);
 }
 
+TEST(Query, RangeTest)
+{
+    std::vector<int> x = range(1, 10).toStdVector();
+    EXPECT_EQ(9, x.size());
+    EXPECT_EQ(2, x[1]);
+    EXPECT_EQ(3, x[2]);
+}
+
+TEST(Query, AnyTest)
+{
+    EXPECT_EQ(true, range(1, 10).any([](int i) {return i == 2; }));
+    EXPECT_EQ(false, range(1, 10).any([](int i) {return i == 12; }));
+}
+
+TEST(Query, AverageTest)
+{
+    auto x = range(1, 4);
+    EXPECT_EQ(std::accumulate(x.begin(), x.end(), 0.0) / x.size(), x.average(0.0));
+}
+
+TEST(Query, SumTest)
+{
+    auto x = range(1, 4);
+    EXPECT_EQ(std::accumulate(x.begin(), x.end(), 0.0) , x.sum(0.0));
+}
+
+TEST(Query, WhereTest)
+{
+    auto x = range(1, 4).where([](int a) {return a < 3; }).toStdVector();
+    EXPECT_EQ(2, x.size());
+}
+
 
 TEST(Query, combinationTest)
 {
-    std::vector<int> x = { 0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9 };
-    std::vector<int> y 
-        = query(x).skip(1).take(3)
+    std::vector<int> x = range(0, 10).toStdVector();
+    std::vector<int> y = query(x)
+        .skip(1).take(3)
         .select([](int a) {return a + 1; })
         .toStdVector();
     EXPECT_EQ(2, y[0]);
